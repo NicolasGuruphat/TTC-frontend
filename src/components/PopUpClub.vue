017bd5772a8831d8256929f9568761a3a4dafe1c
Xan <xanmaris.pro@gmail.com>
2023-01-07 17:47:29 +0100

    prise en main et ajout d'une grille

--- src/components/PopUpClub.vue
+++ src/components/PopUpClub.vue
@@ -2,8 +2,31 @@
     <div id="main-wrapper">
         <button id="close" @click="$emit('switchDisplay')">X</button>
         <div id="information-wrapper">
-           {{club}}
+           <!-- {{club}} -->
+           <label id="nomClub"> nom du club</label>
         </div>
+
+        <div class="wrapper">
+          <div class = "colonne1">
+
+            <div class = "ligne1">
+              <label id="ville"> Ville : </label> 
+              <label id="ville_value"> METTRE VILLE</label>
+            </div>
+
+            <div class = "ligne2">
+            <label id="Fax">Fax : </label>
+            <label id="Fax_value">METTRE NUMERO </label>
+            </div>
+
+          </div>
+          <div class = "colonne2">
+            <label id="ville"> Ville : </label> <label id="ville_value"> METTRE VILLE</label><br>
+            <label id="Fax">Fax : </label>  <label id="Fax_value">METTRE NUMERO </label><br>
+          </div>
+
+
+        </div>  
     </div>
 </template>
 
@@ -35,6 +58,8 @@ export default {
     padding-top: 2.5%;
     color: black;
     font-size: 150%;
+    height: 120px;
+    text-align: center;
     font-family: Calibri;
     font-weight: bold;
 }
@@ -52,4 +77,18 @@ export default {
     border: none;
     background-color: red;
 }
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: auto;
+  grid-gap: 20px;
+}
+
+/* .ligne1 {
+  grid-row: 1;
+}
+.ligne2 {
+  grid-row: 2;
+} */
+
 </style>
