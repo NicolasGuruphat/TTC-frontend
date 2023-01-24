68a565a811bb6f5ff650a223edc78a7a8707efa0
NicolasGuruphat <nicolas.guruphat@gmail.com>
2023-01-24 21:50:27 +0100

    Display markers when store value is updated + add exemple

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,35 +8,63 @@ import { useClubStore } from '@/stores/club';
 import leaflet from "leaflet";  
 
 export default {
-  
+
+  watch: {
+    "store.clubs": {
+      handler: function (val, oldVal) {
+        console.log(val)
+        val.forEach(element=> {
+          console.log(element);
+          console.log(element.location.coordinates);
+          let coords = element.location.coordinates;
+          leaflet.marker([coords[1], coords[0]]).addTo(this.map).on('click', ()=>{
+            this.$emit("setClub", element);
+            this.$emit("displayPopUp", true);
+          });
+        });
+      },
+      deep: true,
+    },
+  },
   props: {
     latitude: Number,
     longtitude: Number,
   
     },
     data(){
-      return {}
+      return {
+        map: null,
+        store: useClubStore()
+      }
     },
-
-  setup(props : any, context : any){
-
-    onMounted(() => {
-      const club = useClubStore();  
-      let map = leaflet.map("map").setView([props.latitude, props.longtitude], 13);
+    mounted(){
+      this.map=leaflet.map("map").setView([this.latitude, this.longtitude], 13);
       leaflet
         .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
           maxZoom: 19,
           attribution:
             '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        })
-        .addTo(map);
-        leaflet.marker([props.latitude, props.longtitude]).addTo(map).on('click', ()=>{
-          context.emit("club_id", 4);
-          context.emit("displayPopUp", true);
-        });
-        leaflet.marker([props.latitude + 0.01, props.longtitude+ 0.01]).addTo(map);
-    });
-  },
+        }).addTo(this.map);
+    },
+  // setup(props : any, context : any){
+    
+    // onMounted(() => {
+    //   context.map = leaflet.map("map").setView([props.latitude, props.longtitude], 13);
+    //   leaflet
+    //     .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    //       maxZoom: 19,
+    //       attribution:
+    //         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    //     })
+    //     .addTo(context.map);
+    //     leaflet.marker([props.latitude, props.longtitude]).addTo(context.map).on('click', ()=>{
+    //       context.emit("club_id", 4);
+    //       context.emit("displayPopUp", true);
+    //     });
+    //     leaflet.marker([props.latitude + 0.01, props.longtitude+ 0.01]).addTo(context.map);
+    //   console.log(context.map);
+    //   });
+  // },
 
   methods:{}
 }
--- src/components/PopUpClub.vue
+++ src/components/PopUpClub.vue
@@ -52,61 +52,13 @@
 
 
 import { defineComponent, onMounted } from "vue";
-import { useClubStore } from '@/stores/club';
 import { mapActions, mapGetters } from "pinia";
 
 
 export default defineComponent({
-  data() {
-
-    return {
-      club : 
-      {
-      "nom": "CD01 - HORS ASSOCIATION - AIN - ARA0001001 - Club",
-      "adresse": "29 Bis Avenue de Bad Kreuznach",
-      "ville": "BOURG-EN-BRESSE",
-      "telephone": "Fax : T\u00e9l bureau : 0474236296",
-      "email": "Email : basket-ain@wanadoo.fr",
-      "site": "Site web : http://www.basketain.com",
-      "salle": [
-          "SALLE JEUNESSE LAIQUE / Mercier",
-          "plan",
-          "42 RUE CHARLES ROBIN",
-          "01000 BOURG-EN-BRESSE",
-          "T\u00e9l : 0474247893 Fax : ",
-          "Couleur des maillots VERT"
-      ],
-      "direction": {
-          "Pr\u00e9sident(e)": [
-              "BRUN Christophe",
-              "01000 BOURG-EN-BRESSE",
-              "T\u00e9l 1 : 0474236296",
-              "T\u00e9l 2 : 0629999317",
-              "email : basket-ain-president@orange.fr"
-          ],
-          "Correspondant(e)": [
-              "DEBIESSE-TIXIER Karine",
-              "39160 CHAZELLES",
-              "T\u00e9l 2 : 0648483362",
-              "email : basket-ain-secretairegeneral@orange.fr"
-          ]
-      },
-      "equipes": [
-          "TIC U13 Masculin 2022 (PIERRE BENITE)",
-          "TIC U13 F\u00e9minin 2022 (PIERRE BENITE)"
-      ]
-}
-    }
+  props:{
+    club: {default:{}}
   },
-  setup() {
-
-    const store = useClubStore()
-    return {store}
-    // onMounted(()=>{
-
-    // })
-  }
-
 })
 
 </script>
